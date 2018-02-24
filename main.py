201b744cbafc44d7405e20020fa7f4e93b12c846
Dustin Tauxe <dtauxe@rams.colostate.edu>
2018-02-24 12:30:10 -0700

    Draws a rect

--- main.py
+++ main.py
@@ -4,20 +4,32 @@
 
 import tkinter as tk
 
+# Main application window
 class Application(tk.Frame):
+    # Constructor
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.pack()
-        self.createWidgets()
+        self.initUI()
 
-    def createWidgets(self):
+    def initUI(self):
+        self.master.title("Viewer")
+
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                                            command=root.destroy)
+        canvasW = 800
+        canvasH = 450
+
+        canvas = tk.Canvas(self, width=canvasW, height=canvasH)
+        rect_color = "#FF00FF"
+        canvas.create_rectangle(10, 20, 400, 300, outline=rect_color, fill=rect_color)
+        canvas.create_rectangle(1, 1, canvasW, canvasH)
+        canvas.pack(fill=tk.BOTH, expand=1)
+
+        self.QUIT = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
         self.QUIT.pack(side="bottom")
 
     def say_hi(self):
